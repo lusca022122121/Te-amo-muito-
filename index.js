ff85f7d384ea1174cc11e32d6a85980c3858b0ef
lusca022122121 <119828624+lusca022122121@users.noreply.github.com>
2022-12-04 23:56:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let container = document.getElementById('container')
+let img = document.getElementById('img')
+let paragraph = document.getElementById('paragrafo')
+let button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    alert("Você é a mulher mais linda, gostosa, maravilhosa, inteligente, esforçada, gentil, educada e tudo de bom nesse mundo ok? Sério você é tudo pra mim e não duvida nunca disso por favor e eu espero do fundo do meu coração conseguir ser o mesmo pra você, você merece tudo de mais maravilhoso!")
+    alert("Eu sou e sempre vou ser apaxionado por você Maria Eduarda (mesmo com você me cortando na foto)!(Assinado Lucas)")
+})  
